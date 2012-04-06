977353a821b5b36ec6a9274cd07fb183078e33c5
Satheesh Ramdass <satheesh.ramdass@gmail.com>
2012-04-06 22:04:58 +0530

    My first commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'jquery-rails'
+gem 'pg'
+gem 'hirb'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'annotate'
+end
+
+group :production, :staging do
+  gem 'thin'
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perfo::Application.config.secret_token = 'db4e7cfc87a53e98b2f1cf728d8c2be6b7bf186219003b2500854d8cd7234acbae19c7bd589dcdaa50ad782ce9b9e8bb36ce0c224812b17c4ad0e5ba9e84a6a6'
