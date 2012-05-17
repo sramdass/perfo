07964e529aa0419cb2e0e5a69004c86b2bcddb79
Satheesh Ramdass <satheesh.ramdass@gmail.com>
2012-05-17 18:58:23 +0530

    Added failed logins and last login time

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,21 @@ class SessionsController < ApplicationController
 
   def create
     profile = UserProfile.find_by_login(params[:login])
-    if profile && profile.authenticate(params[:password])
+    if profile && profile.valid_profile? && profile.authenticate(params[:password])
       if params[:remember_me]
         cookies.permanent[:auth_token] = profile.auth_token
       else
         cookies[:auth_token] = profile.auth_token
       end
-       redirect_to institution_new_path, :notice => "Logged in!"
+      profile.last_login_at = Time.now
+      profile.update_failed_login_count(true)
+      redirect_to institution_new_path, :notice => "Logged in!"
     else
+      profile.update_failed_login_count(false) if profile#Valid profile, but unsuccessful login
       flash.now.alert = "Invalid login or password"
       render :new
     end
+    profile.save! if profile #Save the profile for the failed_login and the last_login only if it is valid
   end
 
   def destroy
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -24,15 +24,18 @@ class UserProfile < TenantManager
   validates_presence_of :user
   
   attr_accessor :password, :profile_type
-  attr_accessible :login, :password, :password_confirmation, :user
+  attr_accessible :login, :password, :password_confirmation, :profile_type, :user
   before_save :encrypt_password
   before_create { generate_token(:auth_token) }
+  before_validation :set_default_values, :on => :create
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_length_of :password, :minimum => 3, :maximum => 50, :on => :create
   validates_presence_of :login
   validates_uniqueness_of :login
+  validates :failed_login_count, :numericality => true, :length => { :minimum => 0 }
+  validates_inclusion_of				:locked,								:in => [true, false]
   
   def authenticate(password)	
     if self.password_hash == BCrypt::Engine.hash_secret(password, self.password_salt)
@@ -61,4 +64,33 @@ class UserProfile < TenantManager
       self[column] = SecureRandom.base64
     end while UserProfile.exists?(column => self[column])
   end  	
+  
+  def update_failed_login_count(login_success)
+  	if login_success == true
+  	   self.failed_login_count = 0
+    else
+      self.failed_login_count = self.failed_login_count + 1;
+    end
+    #Make sure the locking an unlocking happens here
+  end
+  
+  def activated?
+  	self.activated
+  end
+  
+  def locked?
+  	self.locked
+  end  
+  
+  def valid_profile?
+   self.activated? && !self.locked?
+  end
+  
+  private
+  
+  def set_default_values
+    self.failed_login_count ||= 0
+    self.locked ||= false
+    self.activated ||= true
+  end
 end
